f782207fe5962627e767f3c2bb5cd77d9bcf03e1
adexcel171 <godwillokenyi@gmail.com>
2023-09-01 15:37:48 -0700

    changed mongo db connect

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
   images: {
     domains: ["images.pexels.com"],
   },
+  
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true
+    };
+    return config;
+  },
 };
-
 module.exports = nextConfig;
--- src/utils/db.js
+++ src/utils/db.js
@@ -1,6 +1,8 @@
 import mongoose from "mongoose";
 
 const connect = async () => {
+
+  
   try {
     await mongoose.connect(process.env.MONGO);
   } catch (error) {
